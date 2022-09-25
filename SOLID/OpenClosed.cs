82d092d4f61683271ed1f5b2a2c650315c9dde95
abuha <abuha@DESKTOP-DOSBFKK>
2022-09-25 19:49:43 +0600

    Add project files.

---
+++ SOLID/OpenClosed.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    public abstract class Shape
+    {
+        public abstract double Area();
+    }
+
+    public class Rectangle : Shape
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public override double Area()
+        {
+            return Height * Width;
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public override double Area()
+        {
+            return Radius * Radius * 3.1416;
+        }
+    }
+
+    public class AreaCalculation
+    {
+        public double TotalArea(Shape[] shapes)
+        {
+            double totalArea = 0;
+            foreach (var item in shapes)
+            {
+                totalArea += item.Area();
+            }
+            return totalArea;
+        }
+    }
+
+}
---
+++ SOLID/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SOLID
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle[] rectangles = new Rectangle[]
+            {
+                new Rectangle { Height = 10, Width = 5 }
+            };
+
+
+            AreaCalculation areaCalculation = new AreaCalculation();
+            Console.WriteLine(areaCalculation.TotalArea(rectangles));
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
